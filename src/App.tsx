35ad7f7ae8492f317e87e300640cc86fe6cdaa20
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-01 13:49:19 -0400

    Create base shaderate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+ const [color, setColor] = useState<any>('#0b7285');
+
+ const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setColor(e.target.value)
+ }
+
+ const adjust = (color: string, amount: number = 0) => {
+  return '#' + color.replace(/^#/, '').replace(/../g, color => ('0' + Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+ }
+
+ const shades = [
+  -30,
+  -20,
+  -10,
+  5,
+  10,
+  20,
+  30,
+  40,
+  50,
+  60,
+  70,
+  80,
+  90,
+  100,
+  110,
+  120,
+  130,
+  140,
+ ]
+
+ return (
+  <div className="container mx-auto max-w-6xl m-5 text-gray-800">
+   <h1 className="text-3xl pt-8 pb-1 font-medium">Shaderate</h1>
+   <p className="pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iste recusandae.</p>
+
+   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="col-span-full">
+     <label htmlFor="color" className="flex flex-col md:w-1/4 w-100 pb-5">
+      Enter color
+      <input type="text" name="color" placeholder="#e2e2e2" className="p-2 border" onChange={onChange} />
+      <input type="color" name="color" value={color}  className="p-2 border" onChange={onChange} />
+     </label>
     </div>
-  );
-}
 
-export default App;
+    {
+     shades && shades.map(shade => (
+      <div
+       className="flex justify-center flex-col p-2 border-2 border-gray-100 shadow-md rounded-md"
+      >
+       <div className="py-16 bg-red-100" style={{ backgroundColor: adjust(color, shade) }}></div>
+       <p className="pt-2 uppercase text-lg text-gray-500">{adjust(color, shade)}</p>
+      </div>
+     ))
+    }
+   
+   </div>
+  </div>
+ )
+}

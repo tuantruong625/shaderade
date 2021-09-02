6f03252caef5cf7d4a6784e5f00e8c04063394a1
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-02 16:32:00 -0400

    Style color swatch

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ export default function App() {
  }
 
  const shades = [
-  -30,
-  -20,
   -10,
   5,
   10,
@@ -31,24 +29,24 @@ export default function App() {
   130,
   140,
  ]
-
+ 
  return (
-  <div className="container mx-auto max-w-6xl m-5 text-gray-800">
+  <div className="container mx-auto max-w-6xl m-5 px-10 text-gray-800">
    <h1 className="text-3xl pt-8 pb-1 font-medium">Shaderate</h1>
    <p className="pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iste recusandae.</p>
 
    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-    <div className="col-span-full">
+    <div className="col-span-full flex items-center">
      <label htmlFor="color" className="flex flex-col md:w-1/4 w-100 pb-5">
       Enter color
-      <input type="text" name="color" placeholder="#e2e2e2" className="p-2 border" onChange={onChange} />
-      <input type="color" name="color" value={color}  className="p-2 border" onChange={onChange} />
+      <input type="text" name="color" value={color} placeholder="#e2e2e2" className="p-2 border" onChange={onChange} />
      </label>
+     <input type="color" name="color" value={color}  className="ml-2 p-2 border-none rounded-full h-10 w-10"  onChange={onChange} />
     </div>
-
     {
-     shades && shades.map(shade => (
+     shades && shades.map((shade, index) => (
       <div
+       key={index}
        className="flex justify-center flex-col p-2 border-2 border-gray-100 shadow-md rounded-md"
       >
        <div className="py-16 bg-red-100" style={{ backgroundColor: adjust(color, shade) }}></div>
@@ -56,7 +54,6 @@ export default function App() {
       </div>
      ))
     }
-   
    </div>
   </div>
  )

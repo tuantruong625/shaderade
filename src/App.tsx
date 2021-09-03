637bb60ea7d3d31fd2ab82033f2cfca2cc2b91c6
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-03 13:03:21 -0400

    Change to blobs

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
   130,
   140,
  ]
- 
+
  return (
   <div className="container mx-auto max-w-6xl m-5 px-10 text-gray-800">
    <h1 className="text-3xl pt-8 pb-1 font-medium">Shaderate</h1>
@@ -41,16 +41,18 @@ export default function App() {
       Enter color
       <input type="text" name="color" value={color} placeholder="#e2e2e2" className="p-2 border" onChange={onChange} />
      </label>
-     <input type="color" name="color" value={color}  className="ml-2 p-2 border-none rounded-full h-10 w-10"  onChange={onChange} />
+     <input type="color" name="color" value={color} className="ml-2 p-2 border-none rounded-full h-10 w-10" onChange={onChange} />
     </div>
     {
      shades && shades.map((shade, index) => (
       <div
        key={index}
-       className="flex justify-center flex-col p-2 border-2 border-gray-100 shadow-md rounded-md"
+       className="flex justify-center flex-col rounded-b-lg"
       >
-       <div className="py-16 bg-red-100" style={{ backgroundColor: adjust(color, shade) }}></div>
-       <p className="pt-2 uppercase text-lg text-gray-500">{adjust(color, shade)}</p>
+       <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill={adjust(color, shade)} d="M34.8,-55.9C45.2,-47.5,53.7,-38,61.5,-26.5C69.2,-15,76.3,-1.5,74,10.2C71.6,21.9,60,31.9,49.8,42.6C39.6,53.2,31,64.6,19.4,69.7C7.8,74.8,-6.7,73.5,-17.3,66.8C-27.9,60.1,-34.6,47.9,-40.3,37.2C-46,26.5,-50.8,17.2,-58.1,4.8C-65.3,-7.6,-75.1,-23.2,-72.7,-35.1C-70.3,-47.1,-55.6,-55.3,-41.4,-62C-27.3,-68.7,-13.6,-73.7,-0.7,-72.6C12.2,-71.5,24.4,-64.3,34.8,-55.9Z" transform="translate(100 100)" />
+       </svg>
+       <p className="pt-2 uppercase text-lg text-gray-500 p-2 border-2 border-t-0 border-gray-100 rounded-b-lg">{adjust(color, shade)}</p>
       </div>
      ))
     }

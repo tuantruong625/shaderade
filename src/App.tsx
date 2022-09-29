f40ac7bfb8177a092b0add48146e75eaab15677c
Tuan N. Truong <tuan.truong625@gmail.com>
2022-09-29 16:31:55 -0400

    Copy change

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
   <div className="container mx-auto max-w-6xl px-10 text-gray-300 flex justify-center items-center min-h-screen">
    <div className="bg-gray-800 p-5 rounded">
     <div className="mb-5">
-     <h1 className="text-3xl font-medium">Shaderate</h1>
+     <h1 className="text-3xl font-medium">Shaderade</h1>
      <p>Generate color shades</p>
     </div>
     <ColorPicker color={color} handleSetColor={handleSetColor} />
